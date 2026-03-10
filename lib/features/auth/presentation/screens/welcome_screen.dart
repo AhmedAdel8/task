c9052e1fddd83a9928dd21e6fc838dae868ed72c
Ahmed Adel <dola9240@gmail.com>
2026-03-10 23:32:15 +0200

    some edit

--- lib/features/auth/presentation/screens/welcome_screen.dart
+++ lib/features/auth/presentation/screens/welcome_screen.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:task/core/constants.dart';
 import 'package:task/core/widgets/custom_button.dart';
-import 'package:task/features/auth/presentation/widgets/welcome_widget.dart';
 import 'package:task/features/auth/presentation/widgets/already_have_account_widget.dart';
 import 'package:task/features/auth/presentation/widgets/custom_soical_button_widget.dart';
 import 'package:task/features/auth/presentation/widgets/divider_widget.dart';
 import 'package:task/features/auth/presentation/widgets/image_row_widget.dart';
+import 'package:task/features/auth/presentation/widgets/welcome_widget.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -22,9 +22,9 @@ class WelcomeScreen extends StatelessWidget {
               height: 290.h,
               child: Column(
                 children: [
-                  ImageRowWidget(images: row1Images, reverseDirection: false),
+                  ImageRowWidget(images: row1Images, reverseDirection: true),
                   7.verticalSpace,
-                  ImageRowWidget(images: row2Images, reverseDirection: true),
+                  ImageRowWidget(images: row2Images, reverseDirection: false),
                 ],
               ),
             ),
